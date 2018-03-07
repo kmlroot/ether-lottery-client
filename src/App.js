41bfafaaf07b0c781b3e83081abe5aa489dea15b
Mauricio Serna Florez <mauricioserf@gmail.com>
2018-03-06 21:58:23 -0500

    [Add]list metamask accounts.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import web3 from './web3'
+
+import './App.css'
 
 class App extends Component {
   render() {
+    web3.eth.getAccounts()
+      .then(console.log)
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Ether Lottery Client</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
