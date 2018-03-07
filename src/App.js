3a929d85c6149452a289d8b6fd14e6da55a69721
Mauricio Serna Florez <mauricioserf@gmail.com>
2018-03-07 09:13:21 -0500

    [Add] enter to lottery.

--- src/App.js
+++ src/App.js
@@ -10,15 +10,14 @@ class App extends Component {
     manager: '',
     players: [],
     balance: '',
-    value: ''
+    value: '',
+    message: ''
   }
 
   async componentDidMount () {
     const manager = await lottery.methods.manager().call()
     const players = await lottery.methods.getPlayers().call()
-    const balance = await web3.eth.getBalance(
-      lottery.options.address
-    )
+    const balance = await web3.eth.getBalance(lottery.options.address)
 
     this.setState({
       manager,
@@ -32,11 +31,14 @@ class App extends Component {
     
     const accounts = await web3.eth.getAccounts()
 
+    this.setState({ message: 'Waiting on transaction success...' })
+
     await lottery.methods.enter().send({
       from: accounts[0],
       value: web3.utils.toWei(this.state.value, 'ether')
-
     })
+
+    this.setState({ message: 'You have been entered.' })
   }
 
   render () {
@@ -60,6 +62,10 @@ class App extends Component {
           </div>
           <button> Enter </button>
         </form>
+
+        <hr />
+
+        <h1> {this.state.message} </h1>
       </div>
     )
   }
